287555460bcff732c5ff25d1711fd6db47954c4a
karolsoares <karo17soaresu@gmail.com>
2023-04-21 21:44:46 -0300

    retrofit

---
+++ google_maps_in_flutter/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter_windows_3.7.7-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Windows\\Documents\\estudos CC\\Flutter\\TRABALHO G1\\flutter_trab\\APP-Flutter\\google_maps_in_flutter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter_windows_3.7.7-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Windows\\Documents\\estudos CC\\Flutter\\TRABALHO G1\\flutter_trab\\APP-Flutter\\google_maps_in_flutter"
+  "FLUTTER_ROOT=C:\\flutter_windows_3.7.7-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Windows\\Documents\\estudos CC\\Flutter\\TRABALHO G1\\flutter_trab\\APP-Flutter\\google_maps_in_flutter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Windows\\Documents\\estudos CC\\Flutter\\TRABALHO G1\\flutter_trab\\APP-Flutter\\google_maps_in_flutter"
+  "FLUTTER_TARGET=C:\\Users\\Windows\\Documents\\estudos CC\\Flutter\\TRABALHO G1\\flutter_trab\\APP-Flutter\\google_maps_in_flutter\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Windows\\Documents\\estudos CC\\Flutter\\TRABALHO G1\\flutter_trab\\APP-Flutter\\google_maps_in_flutter\\.dart_tool\\package_config.json"
+)
