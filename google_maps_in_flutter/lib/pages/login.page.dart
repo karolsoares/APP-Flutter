5e7144125de2aaacfeebb772904cfb60a5212138
karolsoares <karo17soaresu@gmail.com>
2023-04-28 06:17:25 -0300

    Update login.page.dart

--- google_maps_in_flutter/lib/pages/login.page.dart
+++ google_maps_in_flutter/lib/pages/login.page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -12,35 +11,49 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                autofocus: true,
-                keyboardType: TextInputType.number,
-                style: const TextStyle(color: Colors.white, fontSize: 20),
-                decoration: const InputDecoration(
-                  labelText: "CPF",
-                  labelStyle: TextStyle(color: Colors.white),
+        backgroundColor: Colors.green,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  autofocus: true,
+                  keyboardType: TextInputType.number,
+                  style: const TextStyle(color: Colors.white, fontSize: 20),
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Colors.white),
+                  ),
                 ),
-              ),
-              const Divider(),
-              ButtonTheme(
+                Divider(),
+                TextField(
+                    autofocus: true,
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(color: Colors.blue, fontSize: 30),
+                    decoration: InputDecoration(
+                      labelText: "Senha",
+                      labelStyle: TextStyle(color: Colors.black),
+                    )),
+                Divider(),
+                ButtonTheme(
+                  padding: const EdgeInsets.all(16.0),
                   height: 60.0,
-                  child: RaisedButton(
-                      onPressed: () => {},
-                      child: const Text("Entrar",
-                          style: TextStyle(color: Colors.deepOrange))))
-            ],
+                  child: ElevatedButton(
+                    child: const Text('Entrar'),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        textStyle:
+                            const TextStyle(fontSize: 20, color: Colors.black)),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
 
