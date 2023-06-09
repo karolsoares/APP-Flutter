cfd73702b69d9dd90fcdb0da24d758ad102aa37c
karolsoares <karo17soaresu@gmail.com>
2023-06-09 17:40:28 -0300

    passos inicias com o firebase

--- google_maps_in_flutter/lib/main.dart
+++ google_maps_in_flutter/lib/main.dart
@@ -4,8 +4,11 @@ import 'models/locations.dart' as locations;
 import 'api_retrofit.dart' as retrofit;
 import 'package:dio/dio.dart';
 import 'pages/login.page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
   runApp(const MyLogin());
 }
 
